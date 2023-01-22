a04028fbd251aef9180bd23d4328304c9e7a7e21
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2023-01-22 10:30:35 +0530

    Capture notes into notes.txt

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import multiprocessing as mp
 import time
 import speech_recognition as sr
 import random
+import signal
+import sys
 
 r = sr.Recognizer()
+pool = None
+fh = open("notes.txt", "w+")
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+
+def closeResources():
+    fh.close()
+    pool.close()
 
 def convertSpeechToText(audio):
     #print("Converting speech to text")
@@ -13,43 +25,48 @@ def convertSpeechToText(audio):
     except:
         print("Coundn't understand you!")
     print(query)
+    fh.write(query+". \n")
 
-def record(consumerQueue):
+def record(transcriberQueue):
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=2)
         #print("Set minimum energy threshold to {}".format(r.energy_threshold))
         print("Say something ...")
 
-        for _ in range(5):
-            sleep_for = random.uniform(0.05, 1.0)
+        while True:
             audio = r.listen(source, phrase_time_limit=5)
             print("Adding audio to transcriber ...")
-            consumerQueue.put_nowait(audio)
+            transcriberQueue.put_nowait(audio)
             print("Adding audio to transcriber ... Done")
 
 def recorderStage(q_in, q_out):
     try:
-        print("stage1 started")
+        print("recorderStage started")
         q_in.get()
-        while True:
-            record(q_out)
-        print("stage1 finished")
+        record(q_out)
+        print("recorderStage finished")
     except Exception as e:
-        print("Error in stage1")
+        print("Error in recorderStage")
         print(e)
     return
 
 def transcriberStage(q_in, q_out):
-    print("stage2 started")
-    while True:
-        print("Waiting for audio ...")
-        convertSpeechToText(q_in.get())
-    q_out.put("Conversion complete.\n")
-    print("stage2 finished")
+    print("Hello")
+    try:
+        print("transcriberStage started")
+        while True:
+            print("Waiting for audio ...")
+            convertSpeechToText(q_in.get())
+        q_out.put("Transcribing complete.\n")
+        print("transcriberStage finished")
+    except Exception as e:
+        print("Error in transcriberStage")
+        print(e)
+        
     return
 
 def main():
-
+    print("Real-time Transcriber Started ...")
     pool = mp.Pool()
     manager = mp.Manager()
 
@@ -59,21 +76,28 @@ def main():
     q_transcriber_to_main = manager.Queue()
 
     # launch workers, passing them the queues they need
+    print("Creating worker for recorder ...")
     results_s1 = pool.apply_async(recorderStage, (q_main_to_recorder, q_recorder_to_transcriber))
+    print(results_s1.ready())
+    print("Creating worker for transcriber ...")
     results_s2 = pool.apply_async(transcriberStage, (q_recorder_to_transcriber, q_transcriber_to_main))
+    print(results_s2.ready())
 
+    print("Initiating recording process ...")
     # Send a message into the pipeline
     q_main_to_recorder.put("Start recording.\n")
 
+    print("Awaiting process completion ...")
     # Wait for work to complete
     print(q_transcriber_to_main.get()+"Main completed.")
 
     pool.close()
     pool.join()
-
+    
     print("All done")
 
     return
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     main()
